97f8ec9afaf2bc270c5d94703cf9c51525158512
Wandati <Petersimon801@gmail.com>
2023-07-05 21:08:24 +0300

    updates

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let form1 = document.querySelector("#form")
-
+console.log(form1)
+// Event listener for the SignUp button
 form1.addEventListener('submit',(e)=>{
     e.preventDefault();
+
     const formData = new FormData(form1);
-    // console.log(formData.get('Name'))
     const data = Object.fromEntries(formData);
+    //Posting the NewForm data to our db.json using POST method
     fetch("http://localhost:3000/Users", {
         method: 'POST',
         headers: {
@@ -14,28 +16,7 @@ form1.addEventListener('submit',(e)=>{
     })
     .then(res => res.json())
     .then(data => console.log(data))
-    alert("Account Was Succesfully created!!!")
-    //   .catch(error => console.log(error))
-    // console.log(data);
+    .catch(error => console.log(error))
+    alert("Account Was Succesfully created!!!Proceed to Log in")
+    
 })
-// let form1 = document.querySelector("#form")
-// form1.addEventListener('submit',handleSubmit)
-
-// function handleSubmit(e){
-//     console.log('click')
-//     e.preventDefault();
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(formData)
-//     console.log(data)
-//     fetch("http://localhost:3000/Users",{
-//         method: 'POST' ,
-//         headers: {
-//             'Content-Type' : 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//     .then(res => res.json())
-//     .then(data =>console.log(data))
-//     .catch(error =>console.group(error))
-// }
-
